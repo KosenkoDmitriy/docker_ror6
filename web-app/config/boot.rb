b02908eb48552d40c89ceb7dda58ee22c1ea70dd
DESKTOP-88FIS1B\Dmitry <kosenkodmitryv@gmail.com>
2025-02-02 13:39:55 +0300

    fix docker deployment

--- web-app/config/boot.rb
+++ web-app/config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'logger' # fix $ rake db:migrate (NameError: uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger)
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
